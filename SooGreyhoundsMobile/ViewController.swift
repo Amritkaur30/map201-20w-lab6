a325b51aeb409099ace3affaaf93828b47cc5f5c
defaultprofile <19044558@g200002-19f.local>
2020-01-23 11:31:51 -0500

    Initial Commit

---
+++ SooGreyhoundsMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SooGreyhoundsMobile
+//
+//  Created by Amrit Kaur on 2020-01-23.
+//  Copyright © 2020 Amrit Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
